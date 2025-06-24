0c41270e7759ce31181f454b40a82e4d8112a859
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2025-06-24 15:05:44 +0300

    test: add coef

--- src/modules/BackgroundParallax/BackgroundParallax.ts
+++ src/modules/BackgroundParallax/BackgroundParallax.ts
@@ -7,21 +7,21 @@ export const initBackgroundParallax = () => {
     return
   }
 
-  const movement = 5 // Определите максимальное смещение в процентах
+  const movement = 7 // максимальное смещение в процентах
+  const coeff = 0.9
 
-  // Обработчик события движения мыши
   window.addEventListener('mousemove', (e) => {
     if (window.innerWidth > 769) {
       // Проверка ширины экрана
       const { innerWidth } = window
-      const mouseX = e.clientX / innerWidth // Нормализуем по ширине
+      const mouseX = e.clientX / innerWidth
 
       const initialPositionX = 0
-      const adjustedPositionX = (1 - mouseX) * movement // Инвертируем сдвиг в процентах
+      const adjustedPositionX = (1 - mouseX) * movement
       const newPositionX = initialPositionX + adjustedPositionX
 
       gsap.to(container, {
-        backgroundPosition: `${newPositionX + 100}% 100%, ${newPositionX + 6.5}% 100%, ${newPositionX + 50}% 21%, ${newPositionX + 34}% 50%, ${newPositionX + 100}% 6.6%, 50% bottom, 50% 50%`,
+        backgroundPosition: `${newPositionX + 100}% 100%, ${newPositionX + 6.5}% 100%, ${(newPositionX + 50) * coeff}% 21%, ${(newPositionX + 34) * coeff}% 50%, ${(newPositionX + 100) * coeff}% 6.6%, 50% bottom, 50% 50%`,
         duration: 0.3,
         ease: 'power2.out'
       })
