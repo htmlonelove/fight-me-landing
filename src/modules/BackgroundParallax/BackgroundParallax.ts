278248722c6b94241ff355823d42f189b82d500f
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2025-06-24 22:54:00 +0300

    fix(BackgroundParallax): fix animation logic

--- src/modules/BackgroundParallax/BackgroundParallax.ts
+++ src/modules/BackgroundParallax/BackgroundParallax.ts
@@ -1,30 +1,74 @@
 import { gsap } from 'gsap'
+import { extractNumericValue } from './helpers/extract-numeric-value'
 
-export const initBackgroundParallax = () => {
-  const container = document.querySelector('[data-parallax="hover"]')
+export const initParallaxBackground = () => {
+  const tabletMediaQuery = window.matchMedia('(min-width: 768px)')
+  const parallaxContainer: HTMLDivElement = document.querySelector(
+    '[data-parallax="hover"]'
+  )
 
-  if (!container) {
+  if (!parallaxContainer) {
     return
   }
 
-  const movement = 7 // максимальное смещение в процентах
-  const coeff = 0.9
+  const MAX_MOVEMENT = 7 // максимальное смещение в процентах
+  const computedStyle = window.getComputedStyle(parallaxContainer)
+  const defaultBackgroundPosition = computedStyle.getPropertyValue(
+    '--background-position'
+  )
 
-  window.addEventListener('mousemove', (e) => {
-    if (window.innerWidth > 769) {
-      // Проверка ширины экрана
-      const { innerWidth } = window
-      const mouseX = e.clientX / innerWidth
+  const parsePositionString = (str) => {
+    const pairs = str.split(', ')
+    return pairs.map((pair) => {
+      const [x, y] = pair.split(' ')
+      return { x, y }
+    })
+  }
 
-      const initialPositionX = 0
-      const adjustedPositionX = (1 - mouseX) * movement
-      const newPositionX = initialPositionX + adjustedPositionX
+  let updatedBackgroundPosition = defaultBackgroundPosition
 
-      gsap.to(container, {
-        backgroundPosition: `${newPositionX + 100}% 100%, ${newPositionX + 6.5}% 100%, ${(newPositionX + 50) * coeff}% 21%, ${(newPositionX + 34) * coeff}% 50%, ${(newPositionX + 100) * coeff}% 6.6%, 50% bottom, 50% 50%`,
-        duration: 0.3,
-        ease: 'power2.out'
+  const onMouseMoveHandler = (evt) => {
+    const { innerWidth } = window
+    const mouseXPercentage = evt.clientX / innerWidth
+
+    const initialPositionX = 0
+    const adjustedPositionX = (1 - mouseXPercentage) * MAX_MOVEMENT
+    const newPositionX = initialPositionX + adjustedPositionX
+
+    const positionArray = parsePositionString(defaultBackgroundPosition)
+
+    const calculateNewBackgroundPosition = () => {
+      return positionArray.map((element) => {
+        const { x, y } = element
+        const yNumericValue = extractNumericValue(y)
+        const xNumericValue = extractNumericValue(x)
+        let newBackgroundPosition
+        if (yNumericValue) {
+          newBackgroundPosition = `${xNumericValue + newPositionX}% ${y}`
+        } else {
+          newBackgroundPosition = `${xNumericValue}% ${y}`
+        }
+        return newBackgroundPosition
       })
     }
-  })
+
+    updatedBackgroundPosition = calculateNewBackgroundPosition().join(', ')
+
+    gsap.to(parallaxContainer, {
+      backgroundPosition: updatedBackgroundPosition,
+      duration: 0.3
+    })
+  }
+
+  const checkMediaQueryBreakpoint = () => {
+    if (tabletMediaQuery.matches) {
+      document.addEventListener('mousemove', onMouseMoveHandler)
+    } else {
+      document.removeEventListener('mousemove', onMouseMoveHandler)
+      parallaxContainer.style.backgroundPosition = ''
+    }
+  }
+
+  checkMediaQueryBreakpoint()
+  tabletMediaQuery.addEventListener('change', checkMediaQueryBreakpoint)
 }
--- src/shared/scripts/index.ts
+++ src/shared/scripts/index.ts
@@ -1,13 +1,13 @@
 import { initScaling } from '@scripts/modules/scaling'
 import { initHeader } from '../../modules/Header/Header.ts'
-import { initBackgroundParallax } from 'src/modules/BackgroundParallax/BackgroundParallax.ts'
+import { initParallaxBackground } from 'src/modules/BackgroundParallax/BackgroundParallax.ts'
 
 document.addEventListener(
   'DOMContentLoaded',
   () => {
     initScaling()
     initHeader()
-    initBackgroundParallax()
+    initParallaxBackground()
   },
   true
 )
